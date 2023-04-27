6da4a24bba47d5a206b7a95d24c019efbf83d538
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-04-27 20:08:23 -0300

    Modificando código arduíno

--- Arduino/DHT11.ino
+++ Arduino/DHT11.ino
@@ -1,13 +1,11 @@
 #include "DHT.h"
 #define dht_type DHT11
-int dht_pin = A2;
-DHT dht_1 = DHT(dht_pin, dht_type); 
+int dht_pin = A0;
 
 void setup()
 {
 Serial.begin(9600);
 dht_1.begin();
-pinMode(switch_pin, INPUT);
 }
 void loop() {
 float umidade = dht_1.readHumidity();
@@ -21,4 +19,5 @@ float temperatura = dht_1.readTemperature();
   Serial.print(temperatura);
   Serial.println(";");
 }
+delay(2000);
 }
